8c78cc437ab364f7e6279b696ac1d660ab03f6fa
TMN <t.martin@adsoftware.fr>
2024-01-01 21:39:18 +0100

    init

---
+++ config.lua
@@ -0,0 +1,104 @@
+require("lvim.lsp.manager").setup("emmet_ls")
+require("copilot.suggestion").toggle_auto_trigger()
+
+vim.opt.relativenumber = true
+lvim.format_on_save.enabled = true
+lvim.transparent_window = true
+vim.diagnostic.config {
+  update_in_insert = true
+}
+
+vim.lsp.buf.hover = true
+lvim.keys.normal_mode = {
+  -- Better window movement
+  ["<C-h>"] = "<C-w>h",
+  ["<C-j>"] = "<C-w>j",
+  ["<C-k>"] = "<C-w>k",
+  ["<C-l>"] = "<C-w>l",
+}
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+-- Set the colorscheme
+-- require('colorbuddy').colorscheme('cobalt2')
+lvim.plugins = {
+  {
+    "ThePrimeagen/vim-be-good",
+    event = "InsertEnter",
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+        require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+      end, 100)
+    end,
+  },
+  { "github/copilot.vim" },
+  { "catppuccin/nvim",   name = "catppuccin", priority = 1000 },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end
+  },
+  {
+    "ibhagwan/fzf-lua"
+  },
+  -- { "hrsh7th/cmp-nvim-lsp-signature-help" },
+  {
+    "aca/emmet-ls",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local configs = require("lspconfig/configs")
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+      capabilities.textDocument.completion.completionItem.resolveSupport = {
+        properties = {
+          "documentation",
+          "detail",
+          "additionalTextEdits",
+        },
+      }
+
+      if not lspconfig.emmet_ls then
+        configs.emmet_ls = {
+          default_config = {
+            cmd = { "emmet-ls", "--stdio" },
+            filetypes = {
+              "html",
+              "css",
+              "javascript",
+              "typescript",
+              "eruby",
+              "typescriptreact",
+              "javascriptreact",
+              "svelte",
+              "vue",
+            },
+            root_dir = function(fname)
+              return vim.loop.cwd()
+            end,
+            settings = {},
+          },
+        }
+      end
+      lspconfig.emmet_ls.setup({ capabilities = capabilities })
+    end,
+  }
+}
+
+vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme "catppuccin"
